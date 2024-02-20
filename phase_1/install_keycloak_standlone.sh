e94cae1d5b4fbf89a8d9c6d83befb68a767e6522
root <root@vmi1649846.contaboserver.net>
2024-02-20 14:38:35 +0100

    premire installation keycloak

---
+++ phase_1/install_keycloak_standlone.sh
@@ -0,0 +1,16 @@
+
+sudo apt update 
+
+# installation java
+
+sudo apt install openjdk-17-jre-headless
+
+# telechargment de keycloak
+cd /opt/
+
+sudo wget https://www.keycloak.org/downloads/keycloak-23.0.6.zip.sha1
+
+unzip keycloak-23.0.6.zip -d ./
+
+# lancement de keycloak
+
