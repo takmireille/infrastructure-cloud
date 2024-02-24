0eb2c90107a63bd06aa130eb6f61b03d220deaf0
root <root@vmi1649847.contaboserver.net>
2024-02-25 00:45:36 +0100

    install gcp cli

---
+++ phase_1/essai_gcp/install_gcp_cli.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Télécharger la clé de signature Google Cloud SDK
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo ap
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+curl https://packages.cloud.google.com/apt/doc/a
+
+curl https://packages.cloud.google.com
+
+curl https://packages.cloud.g
+
+curl https://packages
+
+curl https:/
+
+cur
+# Ajouter le dépôt Google Cloud SDK à votre liste de sources APT
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Mettre à jour la liste des paquets disponibles et installer le Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+
+sudo apt-get update && sudo apt-get install google-clou
+
+sudo apt-get update && sudo apt-get install goog
+
+sudo apt-get update && sudo apt-get insta
+
+sudo apt-get update && sudo apt-ge
+
+sudo apt-get update && sud
+
+sudo apt-get updat
+
+sudo apt-g
+
+s
+# Vérifier l'installation
+gcloud version
+
+gcloud vers
+
+gclo
